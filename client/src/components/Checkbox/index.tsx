89dd082570ae48f01fb7499f473f3e1d64c23971
MarceloVBrasil <marcelo.vital.brasil@gmail.com>
2023-01-09 15:24:21 -0300

    create checkbox component

---
+++ client/src/components/Checkbox/index.tsx
@@ -0,0 +1,9 @@
+import "./Checkbox.scss"
+
+export default function Checkbox() {
+    return (
+        <div className="checkbox">
+            <input type='checkbox' name="remember" />Remember Me
+        </div>
+    )
+}
