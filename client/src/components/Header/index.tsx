da9e3433c69900cb213c26b5c652f75e58452e6d
MarceloVBrasil <marcelo.vital.brasil@gmail.com>
2023-01-09 15:57:29 -0300

    create header component

---
+++ client/src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import Contact from "../Contact"
+import "./Header.scss"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <Contact />
+        </header>
+    )
+}
