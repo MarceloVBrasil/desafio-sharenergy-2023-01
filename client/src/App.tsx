50791e9fd6de4e81f66cc9c8b98f19463bb7564d
MarceloVBrasil <marcelo.vital.brasil@gmail.com>
2023-01-09 17:36:10 -0300

    add page routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import { BrowserRouter } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router"
+import Header from "./components/Header";
+import "../src/styles/App.scss"
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import DogsPage from "./pages/DogsPage";
+import CatsPage from "./pages/CatsPage";
+import Navbar from "./components/Navbar";
+import ClientsPage from "./pages/ClientsPage";
+import NewClientPage from "./pages/NewClientPage";
+import EditClientPage from "./pages/EditClientPage";
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dogs" element={<DogsPage />} />
+        <Route path="/cats" element={<CatsPage />} />
+        <Route path="/clients" element={<ClientsPage />} />
+        <Route path="/clients/add" element={<NewClientPage />} />
+        <Route path="/clients/:id" element={<EditClientPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
